38920bb834316c2c2b95b5870d0ed159d564499f
compf <timo.compf@outlook.de>
2024-10-28 14:36:06 +0000

    refactoring

---
+++ ExecutionParameters.java
@@ -0,0 +1,8 @@
+public class ExecutionParameters {
+    private User loginUser;
+    private long projectCode;
+    private int workflowInstanceId;
+    private ExecuteType executeType;
+
+    // Constructor, getters, and setters
+}
--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
@@ -140,10 +140,10 @@ public class TaskInstance implements Serializable {
 
     private int testFlag;
 
-    public void init(String host, Date startTime, String executePath) {
-        this.host = host;
-        this.startTime = startTime;
-        this.executePath = executePath;
+    public void init(TaskInstanceInitParameters initParameters) {
+        this.host = initParameters.getHost();
+        this.startTime = initParameters.getStartTime();
+        this.executePath = initParameters.getExecutePath();
     }
 
 }
