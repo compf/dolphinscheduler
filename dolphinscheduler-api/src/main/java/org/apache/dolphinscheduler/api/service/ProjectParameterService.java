8933b59dcf73ea1a185619198b4e922c63633fc7
compf <timo.compf@outlook.de>
2024-10-28 14:41:41 +0000

    refactoring

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
@@ -22,11 +22,9 @@ import org.apache.dolphinscheduler.dao.entity.User;
 
 public interface ProjectParameterService {
 
-    Result createProjectParameter(User loginUser, long projectCode, String projectParameterName,
-                                  String projectParameterValue, String projectParameterDataType);
+    Result createProjectParameter(ProjectParameterInfo parameterInfo);
 
-    Result updateProjectParameter(User loginUser, long projectCode, long code, String projectParameterName,
-                                  String projectParameterValue, String projectParameterDataType);
+    Result updateProjectParameter(ProjectParameterInfo parameterInfo);
 
     Result deleteProjectParametersByCode(User loginUser, long projectCode, long code);
 
---
+++ org.apache.dolphinscheduler.dao.entity.AlertInformation.java
@@ -0,0 +1,10 @@
+package org.apache.dolphinscheduler.dao.entity;
+
+public class AlertInformation {
+
+    private String title;
+    private String content;
+    private Integer alertGroupId;
+
+    // Constructor, getters, setters, and other methods
+}
