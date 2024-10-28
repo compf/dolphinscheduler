5258046dff98e6b177b4744bdbd729a6a947f9ef
compf <timo.compf@outlook.de>
2024-10-28 09:33:24 +0000

    validation-2

---
+++ org/apache/dolphinscheduler/dao/entity/ExecutionEnvironment.java
@@ -0,0 +1,28 @@
+package org.apache.dolphinscheduler.dao.entity;
+
+import java.util.Date;
+
+public class ExecutionEnvironment {
+
+    private String host;
+    private Date startTime;
+    private String executePath;
+
+    public ExecutionEnvironment(String host, Date startTime, String executePath) {
+        this.host = host;
+        this.startTime = startTime;
+        this.executePath = executePath;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public String getExecutePath() {
+        return executePath;
+    }
+}
