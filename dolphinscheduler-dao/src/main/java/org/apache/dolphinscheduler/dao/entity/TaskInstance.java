d6e93aaf5fc240e496ebf4c870a0c04e308e80d5
compf <timo.compf@outlook.de>
2024-10-28 14:42:39 +0000

    validation-0

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
@@ -140,7 +140,10 @@ public class TaskInstance implements Serializable {
 
     private int testFlag;
 
-public void init(TaskInstanceInitParameters initParameters) {
+public void init(String host, Date startTime, String executePath) {
+        this.host = host;
+        this.startTime = startTime;
+        this.executePath = executePath;
     }
 
 }
--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
@@ -274,7 +274,14 @@ public interface WorkflowInstanceMapper extends BaseMapper<WorkflowInstance> {
      * @param endTime               endTime
      * @return workflow instance IPage
      */
-    IPage<WorkflowInstance> queryWorkflowInstanceListV2Paging(WorkflowInstanceQueryParameters params);
+    IPage<WorkflowInstance> queryWorkflowInstanceListV2Paging(Page<WorkflowInstance> page,
+                                                              @Param("projectCode") Long projectCode,
+                                                              @Param("workflowDefinitionCode") Long workflowDefinitionCode,
+                                                              @Param("name") String name,
+                                                              @Param("startTime") String startTime,
+                                                              @Param("endTime") String endTime,
+                                                              @Param("state") Integer state,
+                                                              @Param("host") String host);
 
     /**
      * Statistics workflow instance state v2
