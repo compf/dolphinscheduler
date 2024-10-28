1ab65e8e103a6148c6a100e481491122101abbfa
compf <timo.compf@outlook.de>
2024-10-28 09:26:27 +0000

    refactoring

---
+++ TaskInstanceInitParameters.java
@@ -0,0 +1,7 @@
+public class TaskInstanceInitParameters {
+    private String host;
+    private Date startTime;
+    private String executePath;
+
+    // Constructor, getters, and setters
+}
---
+++ WorkflowInstanceQueryParameters.java
@@ -0,0 +1,11 @@
+public class WorkflowInstanceQueryParameters {
+    private Long projectCode;
+    private Long workflowDefinitionCode;
+    private String name;
+    private String startTime;
+    private String endTime;
+    private Integer state;
+    private String host;
+
+    // Constructor, getters, and setters
+}
