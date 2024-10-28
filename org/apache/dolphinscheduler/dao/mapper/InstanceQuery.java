f9abd1c8cbc4b6f8fe996fa268626a6577b21e1a
compf <timo.compf@outlook.de>
2024-10-28 09:25:50 +0000

    validation-2

--- org/apache/dolphinscheduler/dao/mapper/InstanceQuery.java
+++ org/apache/dolphinscheduler/dao/mapper/InstanceQuery.java
@@ -1,9 +1,13 @@
 package org.apache.dolphinscheduler.dao.mapper;
 
+import java.util.Date;
+
 public class InstanceQuery {
     private Long projectCode;
-    private String name;
-    private String host;
+    private Long workflowDefinitionCode;
+    private Date startTime;
+    private Date endTime;
+    private Integer state;
 
     // Constructor, getters, and setters
 }
