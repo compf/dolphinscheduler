fdbd3198c545526204b56810b718898a6059e153
compf <timo.compf@outlook.de>
2024-10-28 09:32:22 +0000

    validation-1

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
@@ -280,7 +280,7 @@ public interface WorkflowInstanceMapper extends BaseMapper<WorkflowInstance> {
                                                               @Param("startTime") String startTime,
                                                               @Param("endTime") String endTime,
                                                               @Param("state") Integer state,
-                                                              );
+
 
     /**
      * Statistics workflow instance state v2
