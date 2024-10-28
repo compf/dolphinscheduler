d2a2a34db6c7a7a1f05ec3667c8cbb060867b6b7
compf <timo.compf@outlook.de>
2024-10-28 14:40:04 +0000

    validation-2

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
@@ -24,14 +24,14 @@ public interface ProjectParameterService {
 
     Result createProjectParameter(User loginUser, long projectCode, String projectParameterName,
                                   String projectParameterValue, String projectParameterDataType);
-
+                                  String projectParameterValue, String projectParameterDataType);
+    Result updateProjectParameter(User loginUser, long projectCode, long code, String projectParameterName,
+                                  String projectParameterValue, String projectParameterDataType);
     Result updateProjectParameter(User loginUser, long projectCode, long code, String projectParameterName,
                                   String projectParameterValue, String projectParameterDataType);
 
     Result deleteProjectParametersByCode(User loginUser, long projectCode, long code);
-
-    Result batchDeleteProjectParametersByCodes(User loginUser, long projectCode, String codes);
-
+Result batchDeleteProjectParametersByCodes(User loginUser, long projectCode, String codes);
     Result queryProjectParameterListPaging(User loginUser, long projectCode, Integer pageSize, Integer pageNo,
                                            String searchVal, String projectParameterDataType);
 
