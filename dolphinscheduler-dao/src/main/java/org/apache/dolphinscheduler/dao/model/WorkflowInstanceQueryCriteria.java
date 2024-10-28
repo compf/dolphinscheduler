52d771a3efcc17e596baf4e2524ca200988bdc56
compf <timo.compf@outlook.de>
2024-10-28 14:35:49 +0000

    validation-1

---
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/model/WorkflowInstanceQueryCriteria.java
@@ -0,0 +1,14 @@
+package org.apache.dolphinscheduler.dao.model;
+
+import java.util.Date;
+
+public class WorkflowInstanceQueryCriteria {
+
+    private Long projectCode;
+    private Long workflowDefinitionCode;
+    private String name;
+    private String host;
+    private Date startTime;
+
+    // Constructor, getters and setters
+}
