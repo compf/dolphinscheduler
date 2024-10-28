6c0b14307b5c8a2775d647a3e8a7373f1babfb3c
compf <timo.compf@outlook.de>
2024-10-28 14:42:13 +0000

    validation-1

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
@@ -21,7 +21,6 @@ import org.apache.dolphinscheduler.common.enums.Flag;
 import org.apache.dolphinscheduler.common.enums.Priority;
 import org.apache.dolphinscheduler.common.enums.TaskExecuteType;
 import org.apache.dolphinscheduler.plugin.task.api.enums.TaskExecutionStatus;
-import org.apache.dolphinscheduler.dao.entity.TaskInitInfo;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -141,8 +140,9 @@ public class TaskInstance implements Serializable {
 
     private int testFlag;
 
-    public void init(TaskInitInfo taskInitInfo) {
-        this.host = taskInitInfo.getHost();
+    // The TaskInitInfo class is not present, reverting changes to previous implementation
+    public void init(String host, Date startTime, String executePath) {
+        this.host = host;
         this.startTime = taskInitInfo.getStartTime();
         this.executePath = executePath;
     }
