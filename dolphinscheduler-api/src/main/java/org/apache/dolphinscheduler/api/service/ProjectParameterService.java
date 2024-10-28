2bb9eaec837368e930df2b69fe9f8135ade10ad9
compf <timo.compf@outlook.de>
2024-10-28 14:40:45 +0000

    validation-1

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
@@ -22,10 +22,10 @@ import org.apache.dolphinscheduler.dao.entity.User;
 
 public interface ProjectParameterService {
 
-    Result createProjectParameter(ProjectParameterInfo parameterInfo,
+    Result createProjectParameter(User loginUser, long projectCode, String projectParameterName,
                                   String projectParameterValue, String projectParameterDataType);
 
-    Result updateProjectParameter(ProjectParameterInfo parameterInfo,
+    Result updateProjectParameter(User loginUser, long projectCode, long code, String projectParameterName,
                                   String projectParameterValue, String projectParameterDataType);
 
     Result deleteProjectParametersByCode(User loginUser, long projectCode, long code);
