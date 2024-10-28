e6726321d257700b18e0100b2d63404a69f6b857
compf <timo.compf@outlook.de>
2024-10-28 09:28:45 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/api/test/pages/workflow/ExecutionTaskRequest.java
@@ -0,0 +1,8 @@
+public class ExecutionTaskRequest {
+    private User loginUser;
+    private long projectCode;
+    private int workflowInstanceId;
+    private String startNodeList;
+
+    // Constructor, getters, and setters
+}
---
+++ org/apache/dolphinscheduler/dao/entity/TaskIdentifier.java
@@ -0,0 +1,7 @@
+public class TaskIdentifier {
+    private String name;
+    private Long projectCode;
+    private String host;
+
+    // Constructor, getters, setters, and any additional task-related methods
+}
