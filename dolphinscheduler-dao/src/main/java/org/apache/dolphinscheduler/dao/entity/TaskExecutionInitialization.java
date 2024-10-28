5d13c64480f8c9edcdc39dcab899bdf8290433ef
compf <timo.compf@outlook.de>
2024-10-28 09:31:38 +0000

    validation-1

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskExecutionInitialization.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskExecutionInitialization.java
@@ -2,6 +2,6 @@ public class TaskExecutionInitialization {
     private String host;
     private Date startTime;
     private String executePath;
-import java.util.Date;
+
     // Constructor, getters and setters
 }
