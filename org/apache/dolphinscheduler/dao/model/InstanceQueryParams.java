ea2cbeaa5c3513a022d071d488793640c00650cb
compf <timo.compf@outlook.de>
2024-10-28 14:42:42 +0000

    validation-4

---
+++ org/apache/dolphinscheduler/dao/model/InstanceQueryParams.java
@@ -0,0 +1,24 @@
+package org.apache.dolphinscheduler.dao.model;
+
+public class InstanceQueryParams {
+
+    private Long projectCode;
+    private Long workflowDefinitionCode;
+    private String name;
+    private String startTime;
+    private String endTime;
+    private Integer state;
+    private String host;
+
+    public InstanceQueryParams(Long projectCode, Long workflowDefinitionCode, String name, String startTime, String endTime, Integer state, String host) {
+        this.projectCode = projectCode;
+        this.workflowDefinitionCode = workflowDefinitionCode;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.state = state;
+        this.host = host;
+    }
+
+    // Getters and Setters
+}
