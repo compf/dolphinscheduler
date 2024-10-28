04397b43392bb98cf4817d736f04bef7df439d7c
compf <timo.compf@outlook.de>
2024-10-28 09:25:31 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/dao/mapper/InstanceQueryParams.java
@@ -0,0 +1,9 @@
+package org.apache.dolphinscheduler.dao.mapper;
+
+public class InstanceQueryParams {
+    private Long projectCode;
+    private String name;
+    private String host;
+
+    // Constructor, getters, and setters
+}
