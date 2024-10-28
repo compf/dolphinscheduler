928ddd3fdedcefe7ccabd8388251d40765d027f6
compf <timo.compf@outlook.de>
2024-10-28 14:39:23 +0000

    validation-0

--- dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/model/TaskAlertInfo.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/model/TaskAlertInfo.java
@@ -22,6 +22,8 @@ public class TaskAlertInfo {
     private String title;
 
     private String content;
+    private String content;
+    private Integer alertGroupId;
 
     private Integer alertGroupId;
 
