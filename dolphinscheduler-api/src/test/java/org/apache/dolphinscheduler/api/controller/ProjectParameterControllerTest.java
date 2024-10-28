94cb3712d3020af27afdc7901e6993b91abbcd0a
compf <timo.compf@outlook.de>
2024-10-28 14:39:01 +0000

    refactoring

--- dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/controller/ProjectParameterControllerTest.java
+++ dolphinscheduler-api/src/test/java/org/apache/dolphinscheduler/api/controller/ProjectParameterControllerTest.java
@@ -48,10 +48,9 @@ public class ProjectParameterControllerTest {
     public void testCreateProjectParameter() {
         User loginUser = getGeneralUser();
 
-        Mockito.when(projectParameterService.createProjectParameter(Mockito.any(), Mockito.anyLong(), Mockito.any(),
-                Mockito.any(), Mockito.any())).thenReturn(getSuccessResult());
-        Result result = projectParameterController.createProjectParameter(loginUser, 1, "key", "value",
-                DataType.VARCHAR.name());
+        Mockito.when(projectParameterService.createProjectParameter(Mockito.any())).thenReturn(getSuccessResult());
+        ProjectParameterInfo projectParameterInfo = new ProjectParameterInfo(loginUser, 1, "key", "value", DataType.VARCHAR.name());
+        Result result = projectParameterController.createProjectParameter(projectParameterInfo);
         Assertions.assertEquals(Status.SUCCESS.getCode(), result.getCode());
     }
 
@@ -59,10 +58,9 @@ public class ProjectParameterControllerTest {
     public void testUpdateProjectParameter() {
         User loginUser = getGeneralUser();
 
-        Mockito.when(projectParameterService.updateProjectParameter(Mockito.any(), Mockito.anyLong(), Mockito.anyLong(),
-                Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(getSuccessResult());
-        Result result = projectParameterController.updateProjectParameter(loginUser, 1, 1L, "key", "value",
-                DataType.LONG.name());
+        Mockito.when(projectParameterService.updateProjectParameter(Mockito.any())).thenReturn(getSuccessResult());
+        ProjectParameterUpdateInfo projectParameterUpdateInfo = new ProjectParameterUpdateInfo(loginUser, 1, 1L, "key", "value", DataType.LONG.name());
+        Result result = projectParameterController.updateProjectParameter(projectParameterUpdateInfo);
         Assertions.assertEquals(Status.SUCCESS.getCode(), result.getCode());
     }
 
