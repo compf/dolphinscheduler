724b0b1783ed8c7efb84fc9d93fad83d2aae0f39
compf <timo.compf@outlook.de>
2024-10-28 14:35:27 +0000

    validation-2

---
+++ org/apache/dolphinscheduler/dao/mapper/InstanceQuery.java
@@ -0,0 +1,9 @@
+public class InstanceQuery {
+    private Long projectCode;
+    private Long workflowDefinitionCode;
+    private String startTime;
+    private String endTime;
+    private Integer state;
+
+    // Constructor, getters, and setters
+}
