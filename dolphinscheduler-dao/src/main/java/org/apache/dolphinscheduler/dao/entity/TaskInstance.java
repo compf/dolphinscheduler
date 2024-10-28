0d30d31d11c13ec3909bd7bc77117addf90a61d6
compf <timo.compf@outlook.de>
2024-10-28 09:33:09 +0000

    validation-0

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstance.java
@@ -140,9 +140,10 @@ public class TaskInstance implements Serializable {
 
     private int testFlag;
 
-    public void init(TaskInstanceInitRequest initRequest) {
-        this.host = initRequest.getHost();
-        this.startTime = initRequest.getStartTime();
+    public void init(String host, Date startTime, String executePath) {
+        this.host = host;
+        this.startTime = startTime;
+        this.executePath = executePath;
         this.executePath = initRequest.getExecutePath();
     }
 
