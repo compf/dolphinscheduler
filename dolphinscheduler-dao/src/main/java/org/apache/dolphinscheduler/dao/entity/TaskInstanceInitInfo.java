b7c84c971c93e22191abf9850b0f6498f675f4d9
compf <timo.compf@outlook.de>
2024-10-28 14:39:22 +0000

    validation-3

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstanceInitInfo.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/entity/TaskInstanceInitInfo.java
@@ -1,6 +1,9 @@
 public class TaskInstanceInitInfo {
 
     private String host;
+    import java.util.Date;
+
+    private String host;
     private Date startTime;
     private String executePath;
 
