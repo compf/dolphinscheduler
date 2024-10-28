8d90159ccf0e26f1da0500e2397f659fc37ab703
compf <timo.compf@outlook.de>
2024-10-28 09:31:22 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/plugin/task/api/model/AlertParameter.java
@@ -0,0 +1,10 @@
+package org.apache.dolphinscheduler.plugin.task.api.model;
+
+public class AlertParameter {
+
+    private String title;
+    private String content;
+    private Integer alertGroupId;
+
+    // constructor, getters, and setters
+}
