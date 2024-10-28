d79acb345d4e2b4d906d0436fba331215fa5f177
compf <timo.compf@outlook.de>
2024-10-28 09:25:36 +0000

    validation-3

---
+++ org/apache/dolphinscheduler/dao/model/InstanceStateQuery.java
@@ -0,0 +1,15 @@
+package org.apache.dolphinscheduler.dao.model;
+
+import java.util.Date;
+import java.util.Set;
+
+public class InstanceStateQuery {
+    private Date startTime;
+    private Date endTime;
+    private Long projectCode;
+    private Long workflowCode;
+    private Integer model;
+    private Set<Integer> projectIds;
+
+    // Constructor, getters, and setters
+}
