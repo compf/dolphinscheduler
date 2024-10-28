841e315059bd6498a01b4fa5eaaa781b6faaf7c3
compf <timo.compf@outlook.de>
2024-10-28 09:33:09 +0000

    validation-3

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/ProjectParameterController.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/ProjectParameterController.java
@@ -72,6 +72,7 @@ public class ProjectParameterController extends BaseController {
                                  @RequestParam("projectParameterName") String projectParameterName,
                                  @RequestParam(value = "projectParameterValue") String projectParameterValue,
                                  @RequestParam(value = "projectParameterDataType", defaultValue = "VARCHAR") String projectParameterDataType) {
+        return projectParameterService.createProjectParameter(loginUser, projectCode, projectParameterName, projectParameterValue, projectParameterDataType);
     }
 
     @Operation(summary = "updateProjectParameter", description = "UPDATE_PROJECT_PARAMETER_NOTES")
@@ -90,6 +91,7 @@ public class ProjectParameterController extends BaseController {
                                  @RequestParam("projectParameterName") String projectParameterName,
                                  @RequestParam(value = "projectParameterValue") String projectParameterValue,
                                  @RequestParam(value = "projectParameterDataType") String projectParameterDataType) {
+        return projectParameterService.updateProjectParameter(loginUser, projectCode, code, projectParameterName, projectParameterValue, projectParameterDataType);
     }
 
     @Operation(summary = "deleteProjectParametersByCode", description = "DELETE_PROJECT_PARAMETER_NOTES")
