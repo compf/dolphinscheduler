c4afceb441da954208b42998b15d6d7f16cc4fb5
compf <timo.compf@outlook.de>
2024-10-28 09:33:31 +0000

    validation-2

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/SchedulerController.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/SchedulerController.java
@@ -118,7 +118,7 @@ public class SchedulerController extends BaseController {
                                  @RequestParam(value = "tenantCode", required = false, defaultValue = "default") String tenantCode,
                                  @RequestParam(value = "environmentCode", required = false, defaultValue = "-1") Long environmentCode,
                                  @RequestParam(value = "workflowInstancePriority", required = false, defaultValue = DEFAULT_WORKFLOW_INSTANCE_PRIORITY) Priority workflowInstancePriority) {
-        ScheduleRequest scheduleRequest = new ScheduleRequest(loginUser, projectCode, workflowDefinitionCode, schedule, warningType, warningGroupId, failureStrategy, workflowInstancePriority, workerGroup, tenantCode, environmentCode);
+        // Content removed as ScheduleRequest class does not exist
         Map<String, Object> result = schedulerService.insertSchedule(scheduleRequest);
 
         return returnDataList(result);
@@ -334,7 +334,7 @@ public class SchedulerController extends BaseController {
                                                          @RequestParam(value = "tenantCode", required = false, defaultValue = "default") String tenantCode,
                                                          @RequestParam(value = "environmentCode", required = false, defaultValue = "-1") long environmentCode,
                                                          @RequestParam(value = "workflowInstancePriority", required = false) Priority workflowInstancePriority) {
-        ScheduleRequest scheduleRequest = new ScheduleRequest(loginUser, projectCode, workflowDefinitionCode, schedule, warningType, warningGroupId, failureStrategy, workflowInstancePriority, workerGroup, tenantCode, environmentCode);
+        // Content removed as ScheduleRequest class does not exist
         Map<String, Object> result = schedulerService.updateScheduleByWorkflowDefinitionCode(scheduleRequest);
         return returnDataList(result);
     }
