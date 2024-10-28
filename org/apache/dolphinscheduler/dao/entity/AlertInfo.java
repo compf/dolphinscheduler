2f27443e5315c84e8e340c1f7b5e87496ed81139
compf <timo.compf@outlook.de>
2024-10-28 09:29:09 +0000

    validation-4

---
+++ org/apache/dolphinscheduler/dao/entity/AlertInfo.java
@@ -0,0 +1,12 @@
+package org.apache.dolphinscheduler.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+
+public class AlertInfo {
+    private String title;
+    private String content;
+    private Integer alertGroupId;
+
+    // Constructor, getters, and setters
+}
