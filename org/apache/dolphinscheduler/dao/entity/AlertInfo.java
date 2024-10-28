2fd678ea60b91ddf3b139eecf737aa89382c3e8b
compf <timo.compf@outlook.de>
2024-10-28 09:31:53 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/dao/entity/AlertInfo.java
@@ -0,0 +1,10 @@
+package org.apache.dolphinscheduler.dao.entity;
+
+public class AlertInfo {
+
+    private String title;
+    private String content;
+    private Integer alertGroupId;
+
+    // Getters and Setters
+}
