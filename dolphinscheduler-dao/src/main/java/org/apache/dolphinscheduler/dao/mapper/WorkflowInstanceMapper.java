dc5020992d50b9678f4c301c618308ef2d954c9a
compf <timo.compf@outlook.de>
2024-10-28 09:32:03 +0000

    validation-4

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
@@ -290,7 +290,7 @@ public interface WorkflowInstanceMapper extends BaseMapper<WorkflowInstance> {
      * @param projectIds   projectIds
      * @return ExecuteStatusCount list
      */
-    List<ExecuteStatusCount> countInstanceStateV2(@Param("searchParams") WorkflowInstanceSearchParams searchParams);
+
 
 
     /**
