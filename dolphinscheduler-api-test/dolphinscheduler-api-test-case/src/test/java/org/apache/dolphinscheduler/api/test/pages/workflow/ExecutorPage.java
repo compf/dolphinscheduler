fec439e58a245af9aea0ea2ff9d85d364faac5c4
compf <timo.compf@outlook.de>
2024-10-28 14:41:03 +0000

    refactoring

--- dolphinscheduler-api-test/dolphinscheduler-api-test-case/src/test/java/org/apache/dolphinscheduler/api/test/pages/workflow/ExecutorPage.java
+++ dolphinscheduler-api-test/dolphinscheduler-api-test-case/src/test/java/org/apache/dolphinscheduler/api/test/pages/workflow/ExecutorPage.java
@@ -40,16 +40,9 @@ public class ExecutorPage {
 
     public HttpResponse startWorkflowInstance(User loginUser,
                                               long projectCode,
-                                              long workflowDefinitionCode,
-                                              String scheduleTime,
-                                              FailureStrategy failureStrategy,
-                                              WarningType warningType) {
-        Map<String, Object> params = new HashMap<>();
+                                              WorkflowInstanceDto workflowInstanceDto) {
+        Map<String, Object> params = workflowInstanceDto.toParamsMap();
         params.put("loginUser", loginUser);
-        params.put("workflowDefinitionCode", workflowDefinitionCode);
-        params.put("scheduleTime", scheduleTime);
-        params.put("failureStrategy", failureStrategy);
-        params.put("warningType", warningType);
         Map<String, String> headers = new HashMap<>();
         headers.put(Constants.SESSION_ID_KEY, sessionId);
 
