f64c5f44128027b8be880ece25850fc008556ff4
compf <timo.compf@outlook.de>
2024-10-28 14:35:30 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/dao/entity/TaskInstanceIdentifier.java
@@ -0,0 +1,9 @@
+package org.apache.dolphinscheduler.dao.entity;
+
+public class TaskInstanceIdentifier {
+    private String name;
+    private Long projectCode;
+    private String host;
+
+    // Constructor, getters, and setters
+}
