db6a09cb78f00feef3a746086aea7fc15ff48faf
compf <timo.compf@outlook.de>
2024-10-28 14:35:24 +0000

    validation-3

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/QueryParameters.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/QueryParameters.java
@@ -1,20 +1,23 @@
 package org.apache.dolphinscheduler.dao.mapper;
 
 import java.util.Date;
+import java.util.Set;
 
 public class QueryParameters {
-    private Long projectCode;
-    private Long workflowDefinitionCode;
     private Date startTime;
     private Date endTime;
-    private Integer state;
+    private Long projectCode;
+    private Long workflowCode;
+    private Integer model;
+    private Set<Integer> projectIds;
 
-    public QueryParameters(Long projectCode, Long workflowDefinitionCode, Date startTime, Date endTime, Integer state) {
-        this.projectCode = projectCode;
-        this.workflowDefinitionCode = workflowDefinitionCode;
+    public QueryParameters(Date startTime, Date endTime, Long projectCode, Long workflowCode, Integer model, Set<Integer> projectIds) {
         this.startTime = startTime;
         this.endTime = endTime;
-        this.state = state;
+        this.projectCode = projectCode;
+        this.workflowCode = workflowCode;
+        this.model = model;
+        this.projectIds = projectIds;
     }
 
     // Getters and setters
