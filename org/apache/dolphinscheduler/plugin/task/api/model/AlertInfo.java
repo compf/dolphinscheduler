683a3e348bf102225f76a40dd156a7eeaffc55c2
compf <timo.compf@outlook.de>
2024-10-28 14:35:20 +0000

    validation-0

--- org/apache/dolphinscheduler/plugin/task/api/model/AlertInfo.java
+++ org/apache/dolphinscheduler/plugin/task/api/model/AlertInfo.java
@@ -5,5 +5,33 @@ public class AlertInfo {
     private String content;
     private Integer alertGroupId;
 
-    // Constructor, getters, and setters
+    public AlertInfo(String title, String content, Integer alertGroupId) {
+        this.title = title;
+        this.content = content;
+        this.alertGroupId = alertGroupId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getAlertGroupId() {
+        return alertGroupId;
+    }
+
+    public void setAlertGroupId(Integer alertGroupId) {
+        this.alertGroupId = alertGroupId;
+    }
 }
