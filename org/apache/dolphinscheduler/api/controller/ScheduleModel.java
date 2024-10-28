9fe627283d2affaf829419dbe6615aa1816141a2
compf <timo.compf@outlook.de>
2024-10-28 09:31:49 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/api/controller/ScheduleModel.java
@@ -0,0 +1,8 @@
+public class ScheduleModel {
+    private WarningType warningType;
+    private int warningGroupId;
+    private FailureStrategy failureStrategy;
+    private Priority workflowInstancePriority;
+    // getters and setters
+    // additional methods related to scheduler properties
+}
---
+++ org/apache/dolphinscheduler/api/controller/SchedulerDefaults.java
@@ -0,0 +1,6 @@
+public class SchedulerDefaults {
+    public static final String DEFAULT_WARNING_TYPE = "NONE";
+    public static final String DEFAULT_NOTIFY_GROUP_ID = "1";
+    public static final String DEFAULT_FAILURE_POLICY = "CONTINUE";
+    public static final String DEFAULT_WORKFLOW_INSTANCE_PRIORITY = "MEDIUM";
+}
