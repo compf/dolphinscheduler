b49b664d350136d0a9cbfb00f7dcd94090a5fa9b
compf <timo.compf@outlook.de>
2024-10-28 14:42:51 +0000

    validation-2

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/ProjectParameterController.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/controller/ProjectParameterController.java
@@ -33,13 +33,14 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "PROJECT_PARAMETER_TAG")
+import io.swagger.annotations.*;
+import org.springframework.http.MediaType;
+
+@Api(tags = "PROJECT_PARAMETER_TAG")
 @RestController
-@RequestMapping("projects/{projectCode}/project-parameter")
+@RequestMapping(value = "projects/{projectCode}/project-parameter", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class ProjectParameterController extends BaseController {
 
