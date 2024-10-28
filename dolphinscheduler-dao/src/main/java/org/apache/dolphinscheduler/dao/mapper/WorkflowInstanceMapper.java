9f12b006786566b7fb50381d562c62e4626d7514
compf <timo.compf@outlook.de>
2024-10-28 14:35:46 +0000

    validation-4

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
@@ -273,7 +273,8 @@ public interface WorkflowInstanceMapper extends BaseMapper<WorkflowInstance> {
                                                               @Param("queryRequest") WorkflowInstanceQueryRequest queryRequest,
      * @param endTime               endTime
      * @return workflow instance IPage
-                                                              @Param("endTime") Date endTime);
+                                                              @Param("endTime") Date endTime
+    );
 
     /**
      * Statistics workflow instance state v2
