447396716454c6f94b06366fb76dd65814ab16bd
compf <timo.compf@outlook.de>
2024-10-28 14:41:45 +0000

    validation-0

--- org/apache/dolphinscheduler/plugin/task/api/model/TaskAlertData.java
+++ org/apache/dolphinscheduler/plugin/task/api/model/TaskAlertData.java
@@ -3,5 +3,27 @@ public class TaskAlertData {
     private String content;
     private Integer alertGroupId;
 
-    // Constructor, getters, setters, and additional alert-related methods
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getAlertGroupId() {
+        return alertGroupId;
+    }
+
+    public void setAlertGroupId(Integer alertGroupId) {
+        this.alertGroupId = alertGroupId;
+    }
 }
