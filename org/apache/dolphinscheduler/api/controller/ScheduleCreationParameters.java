297f809f6efe53e3d2761507971432bb7f952a89
compf <timo.compf@outlook.de>
2024-10-28 09:26:45 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/api/controller/ScheduleCreationParameters.java
@@ -0,0 +1,15 @@
+public class ScheduleCreationParameters {
+    private User loginUser;
+    private long projectCode;
+    private long workflowDefinitionCode;
+    private String schedule;
+    private WarningType warningType;
+    private int warningGroupId;
+    private FailureStrategy failureStrategy;
+    private Priority workflowInstancePriority;
+    private String workerGroup;
+    private String tenantCode;
+    private Long environmentCode;
+
+    // Constructor, getters, and setters
+}
