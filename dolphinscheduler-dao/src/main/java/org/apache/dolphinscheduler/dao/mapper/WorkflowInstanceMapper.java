517b42ecb318622e6621c2bacc1d79ad25ee11c0
compf <timo.compf@outlook.de>
2024-10-28 14:38:40 +0000

    validation-3

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
@@ -276,7 +276,7 @@ public interface WorkflowInstanceMapper extends BaseMapper<WorkflowInstance> {
      */
     IPage<WorkflowInstance> queryWorkflowInstanceListV2Paging(Page<WorkflowInstance> page,
                                                               @Param("projectCode") Long projectCode,
-                                                              @Param("queryParameters") WorkflowInstanceQueryParameters queryParameters,
+
                                                               @Param("name") String name,
                                                               @Param("host") String host);
 
@@ -293,8 +293,7 @@ public interface WorkflowInstanceMapper extends BaseMapper<WorkflowInstance> {
      * @param projectIds   projectIds
      * @return ExecuteStatusCount list
      */
-    List<ExecuteStatusCount> countInstanceStateV2(@Param("queryParameters") WorkflowInstanceQueryParameters queryParameters);
-                                                  @Param("startTime") Date startTime,
+    List<ExecuteStatusCount> countInstanceStateV2(@Param("startTime") Date startTime,
                                                   @Param("endTime") Date endTime,
                                                   @Param("projectCode") Long projectCode,
                                                   @Param("workflowCode") Long workflowCode,
