193f08044defffb95adb2290897dcc5d4ead4c2a
compf <timo.compf@outlook.de>
2024-10-28 14:39:30 +0000

    validation-4

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/utils/TaskInstanceUtils.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/utils/TaskInstanceUtils.java
@@ -32,9 +32,9 @@ public class TaskInstanceUtils {
         target.setId(source.getId());
         target.setName(source.getName());
         target.setTaskType(source.getTaskType());
-        // Correcting the assumption that the WorkflowInstance ID is directly accessible
-        // Now retrieving the ID from the WorkflowInstance object
-        target.setWorkflowInstanceId(source.getWorkflowInstance().getId());
+        // Correcting the method to set the workflow instance ID on the target
+        // Assuming that the workflow instance ID is stored in a field named workflowInstanceId in the source object
+        target.setWorkflowInstanceId(source.getWorkflowInstanceId());
         target.setWorkflowInstanceName(source.getWorkflowInstanceName());
         target.setProjectCode(source.getProjectCode());
         target.setTaskCode(source.getTaskCode());
