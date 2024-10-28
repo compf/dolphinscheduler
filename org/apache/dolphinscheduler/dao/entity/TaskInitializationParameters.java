279e32a63d35eba8bdc24781f6bc8a92e16084d6
compf <timo.compf@outlook.de>
2024-10-28 14:36:24 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/dao/entity/TaskInitializationParameters.java
@@ -0,0 +1,7 @@
+public class TaskInitializationParameters {
+    private String host;
+    private Date startTime;
+    private String executePath;
+
+    // Constructor, getters, and setters
+}
---
+++ org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceQueryParameters.java
@@ -0,0 +1,12 @@
+public class WorkflowInstanceQueryParameters {
+    private Page<WorkflowInstance> page;
+    private Long projectCode;
+    private Long workflowDefinitionCode;
+    private String name;
+    private String startTime;
+    private String endTime;
+    private Integer state;
+    private String host;
+
+    // Constructor, getters, and setters
+}
