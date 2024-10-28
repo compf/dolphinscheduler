cb5ab66888482242c4d03f8023aa0e6173307d10
compf <timo.compf@outlook.de>
2024-10-28 14:35:51 +0000

    validation-2

--- dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
+++ dolphinscheduler-dao/src/main/java/org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceMapper.java
@@ -279,10 +279,9 @@ public interface WorkflowInstanceMapper extends BaseMapper<WorkflowInstance> {
                                                               @Param("workflowDefinitionCode") Long workflowDefinitionCode,
                                                               @Param("name") String name,
                                                               @Param("startTime") String startTime,
-                                                              @Param("endTime") String endTime,
+                                                              @Param("endTime") String endTime);
                                                               @Param("state") Integer state,
                                                               @Param("host") String host);
-
     /**
      * Statistics workflow instance state v2
      * <p>
