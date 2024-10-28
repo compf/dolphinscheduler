9a658ba49f11eea8be04f6cc0621406cbc095ecd
compf <timo.compf@outlook.de>
2024-10-28 14:35:39 +0000

    refactoring

--- dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
+++ dolphinscheduler-api/src/main/java/org/apache/dolphinscheduler/api/service/ProjectParameterService.java
@@ -22,11 +22,9 @@ import org.apache.dolphinscheduler.dao.entity.User;
 
 public interface ProjectParameterService {
 
-    Result createProjectParameter(User loginUser, long projectCode, String projectParameterName,
-                                  String projectParameterValue, String projectParameterDataType);
+    Result createProjectParameter(ProjectParameterDetails projectParameterDetails);
 
-    Result updateProjectParameter(User loginUser, long projectCode, long code, String projectParameterName,
-                                  String projectParameterValue, String projectParameterDataType);
+    Result updateProjectParameter(ProjectParameterDetails projectParameterDetails);
 
     Result deleteProjectParametersByCode(User loginUser, long projectCode, long code);
 
