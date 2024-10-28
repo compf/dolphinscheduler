6f93215fd8d89f792204689ff55ea3beb050c1d5
compf <timo.compf@outlook.de>
2024-10-28 14:34:47 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/dao/mapper/WorkflowInstanceQueryCriteria.java
@@ -0,0 +1,9 @@
+package org.apache.dolphinscheduler.dao.mapper;
+
+public class WorkflowInstanceQueryCriteria {
+    private Long projectCode;
+    private String name;
+    private String host;
+
+    // Constructor, getters, and setters
+}
