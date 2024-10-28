e235b316126619d547c6079567a26bb5c3b04f3f
compf <timo.compf@outlook.de>
2024-10-28 09:32:33 +0000

    validation-0

--- dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/model/TaskAlertInfo.java
+++ dolphinscheduler-task-plugin/dolphinscheduler-task-api/src/main/java/org/apache/dolphinscheduler/plugin/task/api/model/TaskAlertInfo.java
@@ -20,14 +20,14 @@ package org.apache.dolphinscheduler.plugin.task.api.model;
 public class TaskAlertInfo {
 
     private String title;
-
+    private String content;
     private String content;
 
     private Integer alertGroupId;
-
+        return title;
     public String getTitle() {
         return title;
-    }
+    public String getTitle() {
 
     public void setTitle(String title) {
         this.title = title;
