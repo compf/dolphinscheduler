10eb77a45e0f8204ec0653ce1d6b83cea66ef327
compf <timo.compf@outlook.de>
2024-10-28 09:27:08 +0000

    refactoring

--- dolphinscheduler-api-test/dolphinscheduler-api-test-case/src/test/java/org/apache/dolphinscheduler/api/test/pages/workflow/ExecutorPage.java
+++ dolphinscheduler-api-test/dolphinscheduler-api-test-case/src/test/java/org/apache/dolphinscheduler/api/test/pages/workflow/ExecutorPage.java
@@ -69,12 +69,12 @@ public class ExecutorPage {
         return requestClient.get(url, headers, params);
     }
 
-    public HttpResponse execute(User loginUser, long projectCode, int workflowInstanceId, ExecuteType executeType) {
+    public HttpResponse execute(ExecutionParameters executionParameters) {
         Map<String, Object> params = new HashMap<>();
-        params.put("loginUser", loginUser);
-        params.put("projectCode", projectCode);
-        params.put("workflowInstanceId", workflowInstanceId);
-        params.put("executeType", executeType);
+        params.put("loginUser", executionParameters.getLoginUser());
+        params.put("projectCode", executionParameters.getProjectCode());
+        params.put("workflowInstanceId", executionParameters.getWorkflowInstanceId());
+        params.put("executeType", executionParameters.getExecuteType());
         Map<String, String> headers = new HashMap<>();
         headers.put(Constants.SESSION_ID_KEY, sessionId);
 
@@ -83,12 +83,11 @@ public class ExecutorPage {
         return requestClient.post(url, headers, params);
     }
 
-    public HttpResponse executeTask(User loginUser, long projectCode, int workflowInstanceId, String startNodeList,
-                                    TaskDependType taskDependType) {
+    public HttpResponse executeTask(ExecutionTaskParameters executionTaskParameters) {
         Map<String, Object> params = new HashMap<>();
-        params.put("loginUser", loginUser);
-        params.put("workflowInstanceId", workflowInstanceId);
-        params.put("startNodeList", startNodeList);
+        params.put("loginUser", executionTaskParameters.getLoginUser());
+        params.put("workflowInstanceId", executionTaskParameters.getWorkflowInstanceId());
+        params.put("startNodeList", executionTaskParameters.getStartNodeList());
         params.put("taskDependType", taskDependType);
         Map<String, String> headers = new HashMap<>();
         headers.put(Constants.SESSION_ID_KEY, sessionId);
