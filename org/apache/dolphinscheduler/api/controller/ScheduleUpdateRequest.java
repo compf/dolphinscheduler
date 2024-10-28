bc0e0c2b69dd044f1e3f245565b3135ed951f3eb
compf <timo.compf@outlook.de>
2024-10-28 09:26:38 +0000

    refactoring

---
+++ org/apache/dolphinscheduler/api/controller/ScheduleUpdateRequest.java
@@ -0,0 +1,13 @@
+package org.apache.dolphinscheduler.api.controller;
+
+import org.apache.dolphinscheduler.common.enums.FailureStrategy;
+import org.apache.dolphinscheduler.common.enums.Priority;
+import org.apache.dolphinscheduler.common.enums.WarningType;
+import org.apache.dolphinscheduler.dao.entity.User;
+
+public class ScheduleUpdateRequest extends ScheduleCreateRequest {
+
+    private long code;
+
+    // getters and setters
+}
---
+++ org/apache/dolphinscheduler/api/service/ProjectParameterUpdateRequest.java
@@ -0,0 +1,10 @@
+package org.apache.dolphinscheduler.api.service;
+
+import org.apache.dolphinscheduler.dao.entity.User;
+
+public class ProjectParameterUpdateRequest extends ProjectParameterRequest {
+
+    private long code;
+
+    // getters and setters
+}
