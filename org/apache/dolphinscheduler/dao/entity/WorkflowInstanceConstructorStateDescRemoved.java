4d2ec8fdeea06c09e2c6f2d6fb728638d7531bef
compf <timo.compf@outlook.de>
2024-10-28 09:31:50 +0000

    validation-2

---
+++ org/apache/dolphinscheduler/dao/entity/WorkflowInstanceConstructorStateDescRemoved.java
@@ -0,0 +1,3 @@
+public class WorkflowInstanceConstructorStateDescRemoved {
+    // The constructor WorkflowInstance(WorkflowDefinition) and method setStateWithDesc have been removed due to errors
+}
